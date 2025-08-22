d6f82a2583272d9cd359509f12f09f244150725b
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-08-22 17:21:40 -0300

    update: integra Prisma nas rotas de usuários

--- backend/routes/usersroutes.js
+++ backend/routes/usersroutes.js
@@ -1,32 +1,68 @@
-import express from "express";
-import { PrismaClient } from '../src/generated/prisma/index.js';
+import express, { request } from "express";
+import { PrismaClient } from "../src/generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
 const authRoutes = express.Router();
-authRoutes.use(express.json())
+authRoutes.use(express.json());
 
-const users = []
-
-authRoutes.post('/users', async (request, response) => {
-  
+authRoutes.post("/users", async (request, response) => {
   await prisma.user.create({
     data: {
       email: request.body.email,
       name: request.body.name,
-      age: request.body.age
+      age: request.body.age,
+    },
+  });
+
+  response.status(201).json(request.body);
+});
+
+authRoutes.put("/users/:id", async (request, response) => {
+  await prisma.user.update({
+    where: {
+      id: request.params.id,
+    },
+
+    data: {
+      email: request.body.email,
+      name: request.body.name,
+      age: request.body.age,
+    },
+  });
+
+  response.status(201).json(request.body);
+});
+
+authRoutes.get("/users", async (request, response) => {
+
+  let users = []
+
+  if (request.query){
+    users = await prisma.user.findMany
+    where: {
+      name: request.query.name
     }
-  })
- 
-  response.status(201).json(request.body)
+    
+  } else {
+
+    const users = await prisma.user.findMany();
+
+  }
+
   
-})
 
-
-authRoutes.get('/users', (request, response) => {
   response.status(200).json(users);
+});
 
+authRoutes.delete("/users/:id", async (request, response) => {
+  await prisma.user.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+
+  response.status(200).json({ message: "O Usuário foi deletado com Sucesso!" });
 });
 
 export default authRoutes;
-
