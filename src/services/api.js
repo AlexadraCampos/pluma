bc15c6d06cd36591418f6b008dc291d56a8bd0b7
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-09-03 11:37:46 -0300

    atualização para o render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/users',
+  baseURL: 'https://pluma-backend.onrender.com/api/users',
 });
+
 
 export default api; 
