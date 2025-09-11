afb2b0b9afd0bb96110e01a148d98d8cb36f6bcf
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-09-11 14:12:25 -0300

    fix: corrigindo nomes de imports

--- src/templates/App.jsx
+++ src/templates/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pluma from "./Pluma.jsx";
-import Cadastro from "./Cadastro.jsx";
+import Cadastro from "./cadastro.jsx";
 import Chat from "./Chat.jsx";
 import Login from "./Login.jsx";
 import Password from "./Password.jsx";
