571051dafdc2907a16e1a512465946a109c9d79d
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-09-11 12:08:05 -0300

    erro do diretorio do login

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setErrorSenha("");
 
     try {
-      const response = await fetch("http://localhost:3000/api/users/login", {
+      const response = await fetch("https://pluma-7rog.onrender.com/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
