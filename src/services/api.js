598fe95eec3805780d1d47e024c20a7eb898d605
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-11-14 18:11:51 -0300

    ajuste diretorio para backend

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://pluma-7rog.onrender.com/api"
-      : "http://localhost:5000/api",
+      ? "https://backend-pluma.onrender.com/api"
+      : "http://localhost:10000/api",
 });    
 
 export default api; 
